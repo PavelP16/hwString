a6073a4b0221180e91107aaf90733eddbe5fb6f6
Pavel <pavelpulle1@gmail.co>
2024-04-28 23:36:07 +0300

    Домашка №1 и несколько вариантов

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,20 @@ public class Main {
         String lastName = "Ivanovich";
         String fullName = firstName + " " + middleName + " " + lastName;
 
-        System.out.printf("«Ф. И. О. сотрудника — %s».\n", fullName);
+        System.out.printf("«Ф. И. О. сотрудника — %s »\n", fullName); // Вариант 1
+        System.out.printf("%s %s %s.\n", firstName, middleName, lastName); // Вариант 2
+
+        String fullName2 = String.format("%s %s %s", firstName, middleName, lastName); // Варинт 3. Метод String
+        System.out.println(fullName2);// Вывод варианта 3
+
+        //Варинт 4 но его редко используют, но знать про него надо.
+        String fullName3 = firstName.concat(" ").concat(middleName).concat(" ").concat(lastName);
+        System.out.println(fullName3);
+
+        //Вариант 5 (не плохой вариант) Хорошо подходит для массива, не нужно циклы городить а можно сразу вывестии все что в массиве
+        String fullName4 = String.join(" ", firstName, middleName, lastName);
+        System.out.println(fullName4);
+
 
         System.out.println("\nЗадача №2________________________________________________________________________");
 
@@ -16,8 +29,10 @@ public class Main {
         System.out.println("\nЗадача №3________________________________________________________________________");
         fullName = "Иванов Семён Семёнович";
         fullName = fullName.replace("ё", "e");
-        System.out.printf("«Данные Ф. И. О. сотрудника — %s».", fullName);
+        System.out.printf("«Данные Ф. И. О. сотрудника — %s».\n", fullName);
 
+        //Вариант 2
+        System.out.println(fullName.replace("ё", "e"));
 
     }
 }
